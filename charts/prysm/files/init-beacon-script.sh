10c02cf4f27a751ce2c7e8254f5475b71512b827
mike.peter.lear@googlemail.com <>
2025-02-25 10:19:56 +0000

    fix: Bracon working - now just Validator

--- charts/prysm/files/init-beacon-script.sh
+++ charts/prysm/files/init-beacon-script.sh
@@ -4,36 +4,59 @@ echo "running script init-beacon-config"
 
 echo "HOSTNAME: $HOSTNAME"
 
+
+{{- if .Values.initChownData.freshInit }}
+echo "Fresh init requested. Removing existing data directories and StateFiles"
 rm -rf /data/network/beacon/$HOSTNAME
 mkdir -p /data/network/beacon/$HOSTNAME
+rm -rf /data/stateFiles
+echo "Removed /data/stateFiles"
+{{- else }}
+echo "Removing existing data directories"
+rm -rf /data/network/beacon/$HOSTNAME
+mkdir -p /data/network/beacon/$HOSTNAME
+{{- end }}
+
+mkdir -p /data/network/beacon/$HOSTNAME
+echo "Created /data/network/beacon/$HOSTNAME"
 mkdir -p /data/stateFiles
+echo "Created /data/stateFiles"
+
+
+ls -lrt /data
 
 
 # Create the genesis state only if it doesn't already exist
 if [ ! -f /data/stateFiles/genesis.ssz ] || [ ! -f /data/stateFiles/genesis-out.json ]; then
 
-  # Download prysmctl if not already available
-  if [ ! -f /usr/local/bin/prysmctl ]; then
-    echo "Downloading prysmctl..."
-    curl -L https://github.com/prysmaticlabs/prysm/releases/download/v5.2.0/prysmctl-v5.2.0-linux-amd64 -o /usr/local/bin/prysmctl
-    chmod +x /usr/local/bin/prysmctl
-  fi
+  echo "Genesis state files not found. Creating..."
+
+  echo "Downloading prysmctl..."
+  curl -L https://github.com/prysmaticlabs/prysm/releases/download/v5.3.0/prysmctl-v5.3.0-linux-amd64 -o /usr/local/bin/prysmctl
+  chmod +x /usr/local/bin/prysmctl
 
   # Check if prysmctl is executable
   ls -lrt /usr/local/bin/prysmctl
 
   echo "Creating the genesis state using the provided genesis.json and config.yml..."
 
-  GENESIS_TIME=${GENESIS_TIME:-1736642736}
+  GENESIS_TIME=$(($(date +%s) + 600))
 
   # Run prysmctl to generate the genesis state
   /usr/local/bin/prysmctl testnet generate-genesis \
-    --num-validators=1 \
+    --num-validators=0 \
     --chain-config-file=/config/config.yml \
     --geth-genesis-json-in=/config/genesis.json \
     --output-ssz=/data/stateFiles/genesis.ssz \
     --geth-genesis-json-out=/data/stateFiles/genesis-out.json \
-    --genesis-time="$GENESIS_TIME"
+    --deposit-json-file=./config/deposit_data-1.json \
+    --genesis-time="$GENESIS_TIME" \
+    --override-eth1data \
+    --execution-endpoint=http://geth-node:8545
+
+  echo "Explicitly checking generated files and keys:"
+  ls -lrt /data/stateFiles
+  ls -lrt /config
 
   echo "Genesis state files created successfully."
 
---
+++ charts/prysm/files/init-validator-script.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+echo "running script init-validator-script"
+
+echo "HOSTNAME: $HOSTNAME"
+
+
+
+# Clean validator data directory
+echo "Cleaning existing validator DB..."
+rm -rf /data/validator/*
+rm -rf /data/validator_keys/*
+mkdir -p /data/validator_keys
+cp -L /config/keystore-m_1.json /data/validator_keys/.
+cp -L /config/password.txt /data/validator_keys/.
+
+ls -lrt /data/validator_keys
+cat /data/validator_keys/keystore-m_1.json
+
+echo "Downloading Prysm Validator binary..."
+curl -L https://github.com/prysmaticlabs/prysm/releases/download/v5.3.0/validator-v5.3.0-linux-amd64 \
+     -o /usr/local/bin/validator
+chmod +x /usr/local/bin/validator
+chmod 644 /data/validator_keys/password.txt
+
+# Import validator keys freshly
+echo "Importing validator keys..."
+/usr/local/bin/validator accounts import \
+  --keys-dir=/data/validator_keys \
+  --wallet-dir=/data \
+  --wallet-password-file=/data/validator_keys/password.txt \
+  --accept-terms-of-use < /dev/null
+
+echo "Validator keys imported successfully."
+
